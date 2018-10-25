f5a84bbe17385491d3eb32b9596d6b1bf3acba77
Taisei-NE30 <ne300085@senshu-u.jp>
2018-10-26 00:27:39 +0900

    [change] DB関連の設定の変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_083935) do
+ActiveRecord::Schema.define(version: 2018_10_25_152424) do
 
   create_table "players", force: :cascade do |t|
     t.string "name"
