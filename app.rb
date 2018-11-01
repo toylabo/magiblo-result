3502f95c0e974005e49a1b837b66dd208da748d7
Taisei-NE30 <ne300085@senshu-u.jp>
2018-11-01 16:56:04 +0900

    [change]isWinの受け取る型を変更

--- app.rb
+++ app.rb
@@ -42,12 +42,14 @@ get '/result/:id' do
             @score_VR = @player.scoreVR
             @score_2D = @player.score2D
             @total = @player.total
-            @is_win_VR = @player.isWinVR.to_bool
-            @is_win_2D = @player.isWin2D.to_bool
+            @result_VR = @player.isWinVR.to_s
+            @result_2D = @player.isWin2D.to_s
             @chara_VR = @player.charaVR.downcase
             @chara_2D = @player.chara2D.downcase
             @restless_str = @player.restlessStr
             @effort_str = @player.effortStr
+
+            
 
             json_comments = open('./public/comments.json') do |io|
                 JSON.load(io)
@@ -56,33 +58,29 @@ get '/result/:id' do
             @chara_VR_JPN = json_comments[@chara_VR]['nameJPN']
             @chara_2D_JPN = json_comments[@chara_2D]['nameJPN']
 
-            if @is_win_VR
+            if @result_VR == "Win"
                 @comment_VR = json_comments[@chara_VR]['messages']['win']
-                @result_VR = "win"
             else
                 @comment_VR = json_comments[@chara_VR]['messages']['lose']
-                @result_VR = "lose"
             end
 
-            if @is_win_2D
+            if @result_2D == "Win"
                 @comment_2D = json_comments[@chara_2D]['messages']['win']
-                @result_2D = "win"
             else
                 @comment_2D = json_comments[@chara_2D]['messages']['lose']
-                @result_2D = "lose"
             end
 
             json_eval = open('./public/eval.json') do |io|
                 JSON.load(io)
             end
 
-            if @is_win_VR && @is_win_2D
+            if isWin?(@result_VR) && isWin?(@result_2D)
                 @eval_messages = json_eval[0]
-            elsif @is_win_VR && !(@is_win_2D)
+            elsif isWin?(@result_VR) && !(isWin?(@result_2D))
                 @eval_messages = json_eval[1]
-            elsif !(@is_win_VR) && @is_win_2D
+            elsif !(isWin?(@result_VR)) && isWin?(@result_2D)
                 @eval_messages = json_eval[2]
-            elsif !(@is_win_VR) && !(@is_win_2D)
+            elsif !(isWin?(@result_VR)) && !(isWin?(@result_2D))
                 @eval_messages = json_eval[3]
             end
 
@@ -98,7 +96,7 @@ get '/result/:id' do
             today_players.each.with_index(1) do |player,index|
                 @today_rank = index if @player.id == player.id
             end
-            makeOGP(@id,@name,@score_VR,@score_2D,@is_win_VR,@is_win_2D,@chara_VR,@chara_2D,@comment_VR,@comment_2D,@all_player_rank,@today_rank,@restless_str,@effort_str)
+            makeOGP(@id,@name,@score_VR,@score_2D,@result_VR,@result_2D,@chara_VR,@chara_2D,@comment_VR,@comment_2D,@all_player_rank,@today_rank,@restless_str,@effort_str)
             erb:index
         else
             error_missing_player
@@ -126,13 +124,13 @@ post '/qr' do
         @score_VR = params[:scoreVR].to_i
         @score_2D = params[:score2D].to_i
         @total = @score_VR + @score_2D
-        @is_win_VR = params[:isWinVR]
-        @is_win_2D = params[:isWin2D]
+        @result_VR = params[:isWinVR]
+        @result_2D = params[:isWin2D]
         @chara_VR = params[:charaVR]
         @chara_2D = params[:chara2D]
         evaluation(params[:moveCount].to_i, @total)
-        @player = Player.new(name: @name, scoreVR: @score_VR, score2D: @score_2D, total: @total, isWinVR: @is_win_VR,
-                             isWin2D: @is_win_2D, charaVR: @chara_VR, chara2D: @chara_2D, restlessStr: @restless_str, effortStr: @effort_str)
+        @player = Player.new(name: @name, scoreVR: @score_VR, score2D: @score_2D, total: @total, isWinVR: @result_VR,
+                             isWin2D: @result_2D, charaVR: @chara_VR, chara2D: @chara_2D, restlessStr: @restless_str, effortStr: @effort_str)
         @player.save
         #@url = "https://result-magiblo.herokuapp.com/result/#{@player.id}"
         #@url = "localhost:4567/result/#{@player.id}"
@@ -205,6 +203,14 @@ helpers do
 
         @effort_str += "☆" * (5 - @effort_str_count)
 
+    end
+
+    isWin?(result) do
+        if result.downcase == "win"
+            true
+        elsif result.downcase == "lose"
+            false
+        end
     end
 
 end
