a8c3727a1a3ed61290fcda50ea81b8fa9481c00c
taichanNE30 <ne300209@senshu-u.jp>
2018-11-02 00:00:26 +0900

    ogp関連タグの出力の修正

--- twit.rb
+++ twit.rb
@@ -8,6 +8,15 @@ def makeTweetLink(id,name,totalScore)
     return "<a href=\"https://twitter.com/intent/tweet?text=#{tweetStr}&url=#{tweetUrl}&hashtags=マジブロ,toy_labo&related=toy_labo\" onClick=\"window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;\" rel=\"nofollow\" class=\"twitter-link\">#{insideAnchor}</a>"        
 end
 
+def makeOGPMeta (id,name,total)
+    "<meta property=\"og:title\" content=\"#{name}さんのマジブロリザルト\" />
+    <meta property=\"og:url\" content=\"http://magiblo-result.herokuapp.com/result/#{id}\" />
+    <meta property=\"og:image\" content=\"http://magiblo-result.herokuapp.com/#{id}.png\" />
+    <meta property=\"og:site_name\" content=\"マジックブロック\" />
+    <meta property=\"og:description\" content=\"#{name}さんの結果です！ #{total}点取りました！!\" />
+    <meta name=\"twitter:image:src\" content=\"http://magiblo-result.herokuapp.com/#{id}.png\">"
+end
+
 def makeOGP(id,name,scoreVR,score2D,isWinVR,isWin2D,charaVR,chara2D,messageVR,message2D,allPlayerRank,todayRank,restlessStr,effortStr)
     framePath = "public/img/Frame.png"
     charaVRImgPath = "public/img/#{charaVR}.png"
