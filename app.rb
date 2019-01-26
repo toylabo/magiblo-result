39f59f7a42270cfb1361617503ca8f1bd4360e44
Taisei-NE30 <ne300085@senshu-u.jp>
2019-01-26 16:03:46 +0900

    nil判定削除,

--- app.rb
+++ app.rb
@@ -110,36 +110,37 @@ get ['/ranking', '/ranking/', '/ranking/:id'] do
 end
 
 post '/qr' do
-    if checkParamsNil?(params)
-        "指定されていないパラメータがあります" 
-    else
-        @name = params[:name]
-        @score_VR = params[:scoreVR].to_i
-        @score_2D = params[:score2D].to_i
-        @total = @score_VR + @score_2D
-        @result_VR = params[:isWinVR]
-        @result_2D = params[:isWin2D]
-        @chara_VR = params[:charaVR]
-        @chara_2D = params[:chara2D]
+    @name = params[:name]
+    @score_VR = params[:scoreVR].to_i
+    @score_2D = params[:score2D].to_i
+    @total = @score_VR + @score_2D
+    @result_VR = params[:isWinVR]
+    @result_2D = params[:isWin2D]
+    @chara_VR = params[:charaVR]
+    @chara_2D = params[:chara2D]
 
-        @chara_2D = 'jasmine' if @chara_2D == 'jasmin'
+    @chara_2D = 'jasmine' if @chara_2D == 'jasmin'
 
-        evaluation(params[:moveCount].to_i, @total)
-        @player = Player.new(name: @name, scoreVR: @score_VR, score2D: @score_2D, total: @total, isWinVR: @result_VR,
-                             isWin2D: @result_2D, charaVR: @chara_VR, chara2D: @chara_2D, restlessStr: @restless_str, effortStr: @effort_str)
+    evaluation(params[:moveCount].to_i, @total)
+    @player = Player.new(name: @name, scoreVR: @score_VR, score2D: @score_2D, total: @total, isWinVR: @result_VR,
+                            isWin2D: @result_2D, charaVR: @chara_VR, chara2D: @chara_2D, restlessStr: @restless_str, effortStr: @effort_str)
+    begin
         @player.save
-        @url = url(@player.id)
-        qr = RQRCode::QRCode.new(@url, :size => 7, :level => :m)
-        @qr = qr.to_img.resize(600,600)
-        @path = "public/qr/#{@player.id}.png"
-        @qr.save(@path)
-        file_content = IO.read(@path)
-        client.upload "/#{@player.id}.png", file_content, :mode => :overwrite
-        @link = client.create_shared_link_with_settings("/#{@player.id}.png")
-        @qr_url = @link.url.sub(/www.dropbox.com/, "dl.dropboxusercontent.com").sub(/\?dl=0/, "")
-        puts @qr_url
-        erb:qr2
+    rescue => error
+        return error
     end
+    
+    @url = url(@player.id)
+    qr = RQRCode::QRCode.new(@url, :size => 7, :level => :m)
+    @qr = qr.to_img.resize(600,600)
+    @path = "public/qr/#{@player.id}.png"
+    @qr.save(@path)
+    file_content = IO.read(@path)
+    client.upload "/#{@player.id}.png", file_content, :mode => :overwrite
+    @link = client.create_shared_link_with_settings("/#{@player.id}.png")
+    @qr_url = @link.url.sub(/www.dropbox.com/, "dl.dropboxusercontent.com").sub(/\?dl=0/, "")
+    puts @qr_url
+    erb:qr2
 end
 
 get '*' do
@@ -156,10 +157,14 @@ helpers do
     end
 
     def url(id)
-        "https://result-magiblo-dev.herokuapp.com/result/" + id.to_s
-        #"localhost:4567/result/" + id.to_s
+        if settings.production?
+            "https://result-magiblo.herokuapp.com/result/" + id.to_s
+        elsif settings.test?
+            "https://result-magiblo-dev.herokuapp.com/result/" + id.to_s
+        else
+            "localhost:4567/result/" + id.to_s
+        end
     end
-
 
     def evaluation(move_count,total)
 
